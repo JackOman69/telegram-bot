ddd5e0af110ac7a550d143689bf9da03f854d9e5
JackOman69 <38285219+JackOman69@users.noreply.github.com>
2023-04-18 19:38:56 +0400

    Add files via upload

---
+++ url_backends.py
@@ -0,0 +1,6 @@
+URL_BACKENDS = {
+    "Arch": "https://arch.fitnesskit-admin.ru/",
+    "TestPy": "https://test-py.fitnesskit-admin.ru/",
+    "Igor": "https://igor.fitnesskit-admin.ru/"
+}
+ENDPOINT = "accounts/save_auth_code/"
